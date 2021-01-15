185f7529b59729cf41e4e4812f7ddf8ea736d4bc
RaidTheWeb <therealraidtheweb@gmail.com>
2021-01-15 16:39:30 +1300

    hi

--- bin/python.js
+++ bin/python.js
@@ -44,20 +44,16 @@ const pyRun = (context) => {
         } 
         function builtinRead(x) {
             if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-                    throw "File not found: '" + x + "'";
+                return
             return Sk.builtinFiles["files"][x];
         }
         
-        // Here's everything you need to run a python program in skulpt
-        // grab the code from your textarea
-        // get a reference to your pre element for output
-        // configure the output function
-        // call Sk.importMainWithBody()
         function runit() { 
         var prog = code;
         Sk.pre = "output";
         Sk.configure({output:outf, read:builtinRead, sysargv: args}); 
         (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+        window.showPrompt = false;
         var myPromise = Sk.misceval.asyncToPromise(function() {
             try {
                 return Sk.importMainWithBody('<stdin>', false, prog, true);
@@ -66,10 +62,14 @@ const pyRun = (context) => {
             }
         });
         myPromise.then(function(mod) {
+            window.showPrompt = true;
+            term.prompt();
             console.log('success');
         },
             function(err) {
             console.log(err.toString());
+            window.showPrompt = true;
+            term.prompt();
         });
         }
         runit()
