385e406cc9fa09ccd9bb5c3fb1000f99da7f62db
RaidTheWeb <raidtheweb@tutanota.com>
2021-01-16 13:31:21 +1300

    Create test.py

---
+++ tests/python/test.py
@@ -0,0 +1,13 @@
+# WebNix Python Tests
+# Turtle Library
+# Turtle Square
+# Test: Passed
+
+import turtle
+
+t = turtle.Turtle()
+
+for c in ['red', 'green', 'blue', 'yellow']:
+    t.color(c)
+    t.forward(75)
+    t.left(90)
